49950db67eaf7f45ae06876b7098a346f54b38bf
angurakanisa31 <angurakanisa.s2023cse@sece.ac.in>
2025-01-11 12:09:32 +0530

    Adding js files in 11.01.25 morg session

--- JavaScript/asyncAwait.js
+++ JavaScript/asyncAwait.js
@@ -1,6 +1,6 @@
 //async await
 
-async function asAw(){
+/*async function asAw(){
     setTimeout(()=>{
         console.log("Inside Timeout")
     },5000)
@@ -9,3 +9,31 @@ async function asAw(){
 asAw().then(function(output){
     console.log(output);
 })
+*/
+// instagram user
+commentcode=async()=>{
+    return new Promise((commentingpost)=>{
+     setTimeout(()=>{
+       commentingpost("comment the post")
+    })
+ })
+ }
+likecode=async()=>{
+   return new Promise((likingpost)=>{
+    setTimeout(()=>{
+      likingpost("liked the post")
+   })
+})
+}
+async function createpost(){
+    var post=new Promise((cpost)=>{
+        setTimeout(()=>{
+            cpost("post created succesfully")
+        },1000)
+    })
+ var[post,likes,comment]=await Promise.all([ post, likecode(),commentcode()])
+ console.log( post)
+ console.log( comment)
+ console.log( likes)
+}
+createpost()
