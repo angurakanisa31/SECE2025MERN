9ff2843cf736ea362bbbf6b58764c9179f91c1c3
angurakanisa31 <angurakanisa.s2023cse@sece.ac.in>
2025-01-17 10:30:33 +0530

    Adding contact and about files

---
+++ seceFrontend/src/component/functional component/About.jsx
@@ -0,0 +1,8 @@
+const About =()=>{
+    return (<div>
+        <h2>Here I'm going to create the about page.I am Angu currently pursuing B.E CSE in 2nd year.
+            And I'm passionate on data science.
+        </h2>
+    </div>);
+};
+export default About;
