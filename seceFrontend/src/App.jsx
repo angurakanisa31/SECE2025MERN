1cb3d89702a4c88170d1d8bd1a2d045676fffef4
angurakanisa31 <angurakanisa.s2023cse@sece.ac.in>
2025-01-21 21:43:05 +0530

    Creating Singup and login page

--- seceFrontend/src/App.jsx
+++ seceFrontend/src/App.jsx
@@ -1,26 +1,54 @@
+
 import Home from"./components/FunctionalComponents/Home"
 import About from "./components/FunctionalComponents/About"
 import Gallery from "./components/FunctionalComponents/Gallery"
 import Contact from "./components/FunctionalComponents/Contact"
 import Navbar from "./components/FunctionalComponents/Navbar"
 import Signup from "./components/FunctionalComponents/Signup"
+import Login from "./components/FunctionalComponents/Login"
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  
+  };
+
   return (<div>
   <BrowserRouter>
+  {isAuthenticated && <Navbar onLogout = {handleLogout}/>}
     <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+      {!isAuthenticated ? (
+          <>
+            <Route path="/" element={<Signup onLogin={handleLogin} />} />
+            <Route path="/Login" element={<Login onLogin={handleLogin} />} />
+      </>
+  ):(
+  <>
+        
+        <Route path='/Home' element={<Home/>}></Route>
         <Route path='/About' element={<About/>}></Route>
         <Route path="/Gallery" element={<Gallery image="SECE logo" page="Gallery"/>}></Route>
         <Route path="/Contact" element={<Contact/>}></Route>
-        <Route path="/Signup" element={<Signup/>}></Route>
-      </Routes>
+        
+        </>
+  )}
+  </Routes>
   </BrowserRouter>
   </div>
       )
-}
+};
 
 export default App;
 
--- seceFrontend/src/components/FunctionalComponents/Signup.jsx
+++ seceFrontend/src/components/FunctionalComponents/Signup.jsx
@@ -1,5 +1,25 @@
-const Signup=()=>{
-    return (<div>
-        <h1>Welcome To Signup Page.</h1>
-    </div>)
-}; 
+import {useNavigate, Link} from "react-router-dom";
+const Signup = (props) => {
+  const navigate = useNavigate();
+
+  const handleSignup = () =>{
+    e.preventDefault();
+    props.onLogin();
+    navigate("/home");
+  }
+
+  return (
+    <div>
+        <h2>Signup</h2>
+        <form onSubmit={handleSignup}>
+          Email : <input type="email" placeholder="Enter your email" />
+          Password : <input type="password" placeholder="password" />
+          Confirm Password : <input type="password" placeholder="password" />
+          <input type="submit" value="submit" />
+        </form>
+        Already have an account?<Link to="/Login" > Login</Link>
+    </div>
+  )
+}
+
+export default Signup
