3abf5f429f079f5321edc7575e84fdd66a22a057
angurakanisa31 <angurakanisa.s2023cse@sece.ac.in>
2025-01-29 11:26:55 +0530

    creating login connection and jwt

--- seceFrontend/src/components/FunctionalComponents/Login.jsx
+++ seceFrontend/src/components/FunctionalComponents/Login.jsx
@@ -1,24 +1,43 @@
-import { useNavigate, Link } from "react-router-dom";
 
-const Login = (props) => {
-const navigate = useNavigate();
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const Login=()=>{
+    var[email,setEmail]=useState("")
+    var[password,setPassword]=useState("")
+    var navigate=useNavigate()
+    const handleLogin= async (event)=>{
+        event.preventDefault()
+console.log("Event Triggered")
+try{const req= await axios.post("http://localhost:3001/login",{
+    email:email,
+    password:password
+})
+//console.log(req)   
+alert(req.data.response);
 
-  const handleLogin = (e) =>{
-    e.preventDefault()
-    props.onLogin();
-    navigate("/Home");
-  }
-  return (
-    <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          Email : <input type="email" placeholder="Enter your email" />
-          Password : <input type="password" placeholder="password" />
-          <input type="submit" value="submit" />
-        </form>
-        Don't have account?<Link to="/" > Signup</Link>
-    </div>
-  )
+    // Navigate only if login is successful
+    if (response.data.isLoggedIn) {
+        navigate("/Home");
+    }
 }
+catch(err){console.log(err)
+    }}
+    return(
+        <div style={{ textAlign: "center" }}>
 
-export default Login
+            <h1 >Signup</h1>
+            <form className="container" method="POST" onSubmit={handleLogin}>
+                <div>
+            Email: 
+                    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/><br /><br />
+            Password:
+                    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+            <br /><br />
+            <button className="button" type="submit" onClick={handleLogin}>Login</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+export default Login;
